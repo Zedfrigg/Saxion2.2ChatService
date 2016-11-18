c9edd0d003afc22bef1d3aaeb6c75298f758a3ef
Reinout Meliesie <zedfrigg@kernelmaft.com>
2016-11-18 17:15:12 +0100

    added more verbose logging

--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -3,13 +3,22 @@ package server;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
+    
     private final Socket socket;
 
     public ClientThread(Socket socket) {
         this.socket = socket;
     }
 
-    public void run(){
+    public void run() {
 
+		boolean running = true;
+		while (running) {
+			if (socket.isConnected()){
+				running = false;
+			}
+		}
+		
+		
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -7,45 +7,44 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+	
+	private final static int LISTEN_PORT = 3000;
+	
     public static void main(String[] args) {
-        Server server = new Server();
-        server.run();
+        
+        new Server().run();
     }
 
     private void run() {
 
-        //initialize the server socket
         ServerSocket serverSocket = null;
-
-
-        try {
-            serverSocket = new ServerSocket(3000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        //try to connect to the server socket at socket 3000
-        while (true) {
-            try {
-
-
-                //continually check for client connections
-                Socket socket = serverSocket.accept();
-
-                //set up input/output stream
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
-
-                //start the client thread
-                ClientThread clientThread = new ClientThread(socket);
-                clientThread.start();
-
-                System.out.println("client connected");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+	
+		System.out.println("Listening on port " + LISTEN_PORT);
+		try {
+			serverSocket = new ServerSocket(LISTEN_PORT);
+		} catch (IOException e) {
+			System.out.println("Couldn't start listening");
+			System.exit(1);
+		}
+		
+		while(true) {
+			try {
+				//continually check for client connections
+				Socket socket = serverSocket.accept();
+				
+				//set up input/output stream
+				InputStream inputStream = socket.getInputStream();
+				OutputStream outputStream = socket.getOutputStream();
+				
+				//start the client thread
+				ClientThread clientThread = new ClientThread(socket);
+				clientThread.start();
+				
+				System.out.println("New client connected with address " + socket.getRemoteSocketAddress());
+			} catch (IOException e) {
+				System.out.println("There was a problem connecting a client");
+			}
+			
+		}
     }
 }
